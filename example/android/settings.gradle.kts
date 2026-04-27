5f3a777f6be03d55980a182db8efa45ee16a05a3
Padraig Nolan <padraig@threadable.io>
2026-04-27 16:53:18 +0100

    chore: update example Android build plugins

--- example/android/settings.gradle.kts
+++ example/android/settings.gradle.kts
@@ -26,10 +26,10 @@ buildscript {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.firebase.crashlytics") version "3.0.3" apply false
+    id("com.android.application") version "9.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.firebase.crashlytics") version "3.0.6" apply false
 }
 
 include(":app")
